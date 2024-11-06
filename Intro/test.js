618db73c58ae532dcee20f14d88afc9388d38aa7
Am1d1v <dimadiman1adima@gmail.com>
2024-11-06 17:19:02 +0300

    Smoke Testing

--- Intro/test.js
+++ Intro/test.js
@@ -1,10 +1,12 @@
 import http from 'k6/http';
+import { sleep } from 'k6';
 
 export const options = {
     vus: 10,
-    duration: '10s'
+    duration: '3s',
 };
 
 export default function(){
     http.get('https://test.k6.io');
+    sleep(1);
 };
