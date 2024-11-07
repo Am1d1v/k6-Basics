9a6841129877d396f11f5ee4d315962b069f5fa1
Am1d1v <dimadiman1adima@gmail.com>
2024-11-07 15:19:06 +0300

    HTTP GET Request Assertions

--- Test Scripts/HTTP-Request.js
+++ Test Scripts/HTTP-Request.js
@@ -1,18 +1,19 @@
 import http from 'k6/http';
-
+import {check} from 'k6'
 
 
 // Positive case
-// export default () => {
-//     const res = http.get('https://test.k6.io');
-//     console.log(res.status);
-
-// };
+export default () => {
+    const res = http.get('https://test.k6.io');
+    check(res, {
+        'response status code is 200': (response) => response.status === 200
+    });
+};
 
 
 // Unexisting url
-export default () => {
+/* export default () => {
     const res = http.get('https://test.k6.io/unexistingurl');
     console.log(res.status);
-};
+}; */
 
