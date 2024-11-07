be6aedcfcdf87b313965f7363591cdd1fc8ad32c
Am1d1v <dimadiman1adima@gmail.com>
2024-11-07 22:36:19 +0300

    Setting Thresholds

--- Test Scripts/HTTP-Request.js
+++ Test Scripts/HTTP-Request.js
@@ -2,15 +2,20 @@ import http from 'k6/http';
 import {check} from 'k6'
 
 
+export const options = {
+    vus: 45,
+    duration: '10s',
+    thresholds: {
+        http_req_duration: ['p(95) < 120']
+    }
+}
+
 // Positive case
 export default () => {
     const res = http.get('https://test.k6.io');
 
     check(res, {
-        'response status code is 200': (response) => response.status === 200
-    });
-
-    check(res, {
+        'response status code is 200': (response) => response.status === 200,
         'page is start page': (response) => response.body.includes('Collection of simple web-pages suitable for load testing.')
     });
 };
