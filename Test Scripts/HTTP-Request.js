1f25f9dd8750ac045eb2fa1cd790812ffa0879a4
Am1d1v <dimadiman1adima@gmail.com>
2024-11-07 22:24:24 +0300

    Validating Responses

--- Test Scripts/HTTP-Request.js
+++ Test Scripts/HTTP-Request.js
@@ -5,8 +5,13 @@ import {check} from 'k6'
 // Positive case
 export default () => {
     const res = http.get('https://test.k6.io');
+
     check(res, {
         'response status code is 200': (response) => response.status === 200
+    });
+
+    check(res, {
+        'page is start page': (response) => response.body.includes('Collection of simple web-pages suitable for load testing.')
     });
 };
 
