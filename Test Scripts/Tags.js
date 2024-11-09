05a4d5f55702c616363b9df5cd25667f49f857b4
Am1d1v <dimadiman1adima@gmail.com>
2024-11-09 15:20:38 +0300

    K6 Tags

---
+++ Test Scripts/Tags.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+
+const options = {
+    thresholds: {
+        http_req_duration: ['p(95) < 120']
+    }
+};
+
+export default () => {
+    http.get('https://run.mocky.io/v3/51396d9d-cdae-431d-94a4-ffd70c0a9ee5');
+    http.get('https://run.mocky.io/v3/7ea53017-8f09-4971-8705-f1c85e324f1d?mocky-delay=2000ms');
+};
