97d98fd139407e9abbf6758a5607ead6bb0e905d
Am1d1v <dimadiman1adima@gmail.com>
2024-11-10 14:44:15 +0300

    k6 Custom Tags

--- Test Scripts/Custom-Tags.js
+++ Test Scripts/Custom-Tags.js
@@ -6,14 +6,21 @@ import { check, sleep } from 'k6';
 
 export const options = {
     thresholds: {
-        http_req_duration: ['p(95) < 300']
+        http_req_duration: ['p(95) < 300'],
+        https_errors: ['count === 0']
     }
 };
 
 let httpErrors = new Counter('https_errors');
 
 export default () => {
-    http.get('https://run.mocky.io/v3/51396d9d-cdae-431d-94a4-ffd70c0a9ee5');
-    http.get('https://run.mocky.io/v3/7ea53017-8f09-4971-8705-f1c85e324f1d');
-    //http.get('https://run.mocky.io/v3/7ea53017-8f09-4971-8705-f1c85e324f1d?mocky-delay=2000ms');
+    let response = http.get('https://run.mocky.io/v3/51396d9d-cdae-431d-94a4-ffd70c0a9ee5');
+    
+    if(response.error){
+        httpErrors.add(1);
+    }
+
+    check(response, {
+        'status is 200': response => response.status === 200
+    })
 };
