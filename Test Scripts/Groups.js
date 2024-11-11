a8ec2159e45c5e46628daf63974fe054b04716b8
Am1d1v <dimadiman1adima@gmail.com>
2024-11-11 14:46:07 +0300

    Groups duration metric

--- Test Scripts/Groups.js
+++ Test Scripts/Groups.js
@@ -4,9 +4,9 @@ import {sleep, check, group} from 'k6';
 
 export const options = {
     thresholds: {
-        'group_duration{group:::Main page}': ['p(95) < 900'],
-        'group_duration{group:::Main page::Assets}': ['p(95) < 300'],
-        'group_duration{group:::News page}': ['p(95) < 600'],
+        'group_duration{group:::Main page}': ['p(95) < 8000'],
+        'group_duration{group:::Main page::Assets}': ['p(95) < 3000'],
+        'group_duration{group:::News page}': ['p(95) < 6000'],
     }
 };
 
@@ -15,22 +15,22 @@ export default () => {
     // Main page group organizing
     group('Main page', () => {
 
-        let res = http.get('https://test.k6.io');
+        let res = http.get('https://run.mocky.io/v3/8643b388-eded-4cce-9007-98468036bd28?mocky-delay=5000ms');
 
         check(res, {
             'status is 200': response => response.status === 200
         });
 
         group('Assets', () => {
-            http.get('https://test.k6.io/static/css/site.css');
-            http.get('https://test.k6.io/static/js/prisms.js');
+            http.get('https://run.mocky.io/v3/8643b388-eded-4cce-9007-98468036bd28?mocky-delay=1000ms');
+            http.get('https://run.mocky.io/v3/8643b388-eded-4cce-9007-98468036bd28?mocky-delay=1000ms');
         });
         
     });
 
     // News page group organizing
     group('News page', () => {
-        http.get('https://test.k6.io/news.php');
+        http.get('https://run.mocky.io/v3/8643b388-eded-4cce-9007-98468036bd28?mocky-delay=5000ms');
     });
     
     sleep(1);
