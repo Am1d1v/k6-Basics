42b7198614f30c8fce209af43f3873671bac1f33
Am1d1v <dimadiman1adima@gmail.com>
2024-11-12 14:46:49 +0300

    GET Request Assertions

--- API HTTP Request/GET-Request.js
+++ API HTTP Request/GET-Request.js
@@ -1,10 +1,18 @@
 import http from 'k6/http'
+import { check } from 'k6';
+
 
 export const options = {
 
 };
 
 export default () => {
-    const res = http.get('https://test-api.k6.io/public/crocodiles/');
-    console.log(res);
+    let res = http.get('https://test-api.k6.io/public/crocodiles/');
+
+    res = http.get('https://test-api.k6.io/public/crocodiles/1');
+
+    check(res, {
+        'status is 200': response => response.status === 200,
+        'Name is Bert': response => response.body.includes('Bert')
+    });
 };
