e862417adb603939c813109619bdd3e076cbf4e2
Am1d1v <dimadiman1adima@gmail.com>
2024-11-13 15:09:15 +0300

    Corellating requests

--- API HTTP Request/GET-Request.js
+++ API HTTP Request/GET-Request.js
@@ -8,13 +8,16 @@ export const options = {
 
 export default () => {
     let res = http.get('https://test-api.k6.io/public/crocodiles/');
+    const crocodiles = res.json();
+    console.log(crocodiles[0].name);
 
-    const crocodileID = 1;
-
+    const crocodileID = crocodiles[0].id;
+    const crocodileName = crocodiles[0].name;
     res = http.get(`https://test-api.k6.io/public/crocodiles/${crocodileID}`);
 
     check(res, {
         'status is 200': response => response.status === 200,
-        'Name is Bert': response => response.json().name === 'Bert'
+        'Name is Bert': response => response.json().name === 'Bert',
+        'Id is equal 1': response => response.json().id = crocodileID
     });
 };
