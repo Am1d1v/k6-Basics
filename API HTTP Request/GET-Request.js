22503eda6288beea7a0f7d1bb079d80d3aa52924
Am1d1v <dimadiman1adima@gmail.com>
2024-11-13 15:17:29 +0300

    Accessing headers

--- API HTTP Request/GET-Request.js
+++ API HTTP Request/GET-Request.js
@@ -8,12 +8,16 @@ export const options = {
 
 export default () => {
     let res = http.get('https://test-api.k6.io/public/crocodiles/');
+    // List of all itams
     const crocodiles = res.json();
-    console.log(crocodiles[0].name);
 
+    // Certain item's value
     const crocodileID = crocodiles[0].id;
     const crocodileName = crocodiles[0].name;
     res = http.get(`https://test-api.k6.io/public/crocodiles/${crocodileID}`);
+
+    console.log(res.headers.Allow);
+    console.log(res.headers['Content-Type']);
 
     check(res, {
         'status is 200': response => response.status === 200,
