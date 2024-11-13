916bb75e1a20209db7611d332433cc6e631ea4b7
Am1d1v <dimadiman1adima@gmail.com>
2024-11-13 15:45:16 +0300

    Making a POST Requst

--- API HTTP Request/POST-Request.js
+++ API HTTP Request/POST-Request.js
@@ -6,9 +6,19 @@ export const options = {
 
 };
 
+const body = JSON.stringify(
+    {
+        username: 'k6TestName1',
+        password: 'k6TestPassword1'
+    },
+)
+
+const params = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
 export default () => {
-    http.post('https://test-api.k6.io/user/register/', {
-        username: 'k6TestName',
-        password: 'k6TestPassword'
-    });
+    http.post('https://test-api.k6.io/user/register/', body, params);
 };
