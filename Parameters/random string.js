11b96aa32678a8b39714ccde2f80b9dfec4eb4d7
Am1d1v <dimadiman1adima@gmail.com>
2024-11-16 16:56:05 +0300

    Random string user register

--- Parameters/random string.js
+++ Parameters/random string.js
@@ -4,23 +4,24 @@ import {randomString} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
  export const options = {
     vus: 10,
-    duration: '15s'
+    duration: '3s'
 }; 
 
 
 export default () => {
     
-    const randomUsername = randomString(10);
-    const randomSecret = randomString(15);
-
     const credentials = {
-        username: 'username_' + Date.now(),
-        password: 'secret_' + Date.now()
+        username: 'username_' + randomString(10),
+        password: 'secret_' + randomString(15)
     };
 
     http.post(
         'https://test-api.k6.io/user/register/',
-        credentials
+        JSON.stringify(credentials),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
         );
-
 };
