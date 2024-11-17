6730ae67b82a5c7945903f552ecd641bfb3b581d
Am1d1v <dimadiman1adima@gmail.com>
2024-11-17 16:20:39 +0300

    k6 Cloud Execution

---
+++ Parameters/using external json.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import {randomString} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import users from './users.json'
+
+ export const options = {
+    vus: 10,
+    duration: '3s'
+}; 
+
+
+export default () => {
+    
+    const credentials = {
+        username: 'username_' + randomString(10),
+        password: 'secret_' + randomString(15)
+    };
+
+    http.post(
+        'https://test-api.k6.io/user/register/',
+        JSON.stringify(credentials),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        );
+};
